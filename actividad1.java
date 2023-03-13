fc4f6fa3fc7647349ffda8bd7c27b0d533e275a0
Naahuel9 <126790743+Naahuel9@users.noreply.github.com>
2023-03-13 11:56:48 -0300

    Add files via upload

---
+++ actividad1.java
@@ -0,0 +1,58 @@
+package Paquete;
+
+public class actividad1 {
+	public static void main(String[]args) {
+		
+
+	//int a=5, b=14;
+	
+	
+	//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+	
+	/*while(a<=b) {
+		System.out.print(a + " ");
+		a++;
+	}*/
+	
+	//BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+	
+	/*while(a<=b) {
+		if(a%2==0) {
+			System.out.print(a + " ");
+		}
+		
+		a++;
+	}*/
+		
+	//CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+	
+	//boolean imprimirPares = false;
+	
+	/*while(a<=b) {
+		if(imprimirPares==true) {
+			if(a%2==0) {
+				System.out.print(a + " ");
+			}
+		}
+		else {
+			if(a%2!=0) {
+			System.out.print(a + " ");
+			}
+		}	
+			a++;
+	}*/	
+	
+	//DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+	
+	/*for(int a=14, b=5;a>=b; a--) {
+		if(a%2==0) {
+		System.out.print(a + " ");
+		}
+	  }*/
+		
+		
+		
+		
+		
+	}
+}
---
+++ actividad2.java
@@ -0,0 +1,34 @@
+package Paquete;
+
+public class actividad2 {
+	public static void main(String[]args) {
+		
+	/*int ingresosM = 500000, vehiculos = 4, inmuebles = 4, otros = 2;
+	
+	
+	if(ingresosM >=489083) {
+			if(vehiculos>=3) {
+				if(inmuebles>=3) {
+					if(otros>=1) {
+							System.out.println("Es una persona con ingresos Altos");
+					}
+				}
+			}
+	}
+		
+	
+	else {
+		System.out.println("No es una persona con Ingresos altos");
+	}*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
